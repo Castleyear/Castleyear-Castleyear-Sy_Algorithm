5dfac8f717b51fe4f27fb978e2ea35913928baf1
Castleyeon <tmfprl06@naver.com>
2023-02-22 02:41:06 +0900

    1 ~ 7일차 알고리즘

--- String/회문문자열.java
+++ String/회문문자열.java
@@ -1,6 +1,4 @@
 package String;
-
-ㄴpackage 알고리즘.String;
 import java.util.*;
 
 public class 회문문자열 {
@@ -46,4 +44,60 @@ public class 회문문자열 {
 		System.out.println(result);
 
 	}
+
+    static class 공통원소구하기 {
+        public List<Integer> solution(int arr1[], int arr2[]) {
+            List<Integer> answer = new ArrayList<>();
+            int i = 0;
+            int j = 0;
+
+            // 배열 오름차순 정렬
+            Arrays.sort(arr1);
+            Arrays.sort(arr2);
+
+            // 1 2 3 4 5
+            // 2 3 4 5 6
+
+            while (i < arr1.length && j < arr2.length) {
+                if (arr1[i] < arr2[j]) {
+                    i++;
+                } else if (arr1[i] > arr2[j]) {
+                    j++;
+                } else {
+                    answer.add(arr1[i++]);
+                    j++;
+                }
+            }
+
+            return answer;
+        }
+
+        public static void main(String[] args) {
+            String.회문문자열.공통원소구하기 main = new String.회문문자열.공통원소구하기();
+            Scanner sc = new Scanner(System.in);
+
+            int n = sc.nextInt();
+            int arr1[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr1[i] = sc.nextInt();
+            }
+
+            int m = sc.nextInt();
+            int arr2[] = new int[m];
+            for (int i = 0; i < m; i++) {
+                arr2[i] = sc.nextInt();
+            }
+
+            // System.out.println("arr1" + Arrays.toString(arr1));
+            // System.out.println("arr2" + Arrays.toString(arr2));
+
+            // n = 열 , m = 행
+            List<Integer> result = main.solution(arr1, arr2);
+
+            for (int x : result) {
+                System.out.print(x + " ");
+            }
+
+        }
+    }
 }
